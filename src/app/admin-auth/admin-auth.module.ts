3a1d3ed5720718c4a57b80f45a526b75b9a8e6e5
Aniruddha-12 <payas.sanjay@gmail.com>
2025-08-03 00:19:23 +0530

    change in adminAuthModule

--- src/app/admin-auth/admin-auth.module.ts
+++ src/app/admin-auth/admin-auth.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 import { AdminSignInComponent } from './admin-sign-in/admin-sign-in.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent,
     AdminSignInComponent
   ],
   imports: [
