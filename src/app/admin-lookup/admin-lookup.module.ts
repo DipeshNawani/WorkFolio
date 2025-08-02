28743d923ba1a4a94c34f42dddb2ba1d8ebf3046
Aniruddha-12 <payas.sanjay@gmail.com>
2025-08-02 19:40:12 +0530

    final edit to base files

--- src/app/admin-lookup/admin-lookup.module.ts
+++ src/app/admin-lookup/admin-lookup.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SalaryComponent } from './salary/salary.component';
 import { ReportComponent } from './report/report.component';
+import { AdminWorkComponent } from './admin-work/admin-work.component';
 
 
 
 @NgModule({
   declarations: [
     SalaryComponent,
-    ReportComponent
+    ReportComponent,
+    AdminWorkComponent
   ],
   imports: [
     CommonModule
