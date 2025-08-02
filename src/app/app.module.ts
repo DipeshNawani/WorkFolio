75e359de8b9524979e11fc6bfe5ce34b7a46c559
Aniruddha-12 <payas.sanjay@gmail.com>
2025-08-02 18:17:00 +0530

    removed the analytics module and added it as a component inside working module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
 import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnalyticsComponent,
     LandingComponent
   ],
   imports: [
--- src/app/working/working.module.ts
+++ src/app/working/working.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogTaskComponent } from './log-task/log-task.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    LogTaskComponent
+    LogTaskComponent,
+    AnalyticsComponent
   ],
   imports: [
     CommonModule
